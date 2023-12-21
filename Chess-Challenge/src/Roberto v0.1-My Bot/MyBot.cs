210ed245e8f0f811be7b45d37fbc96ed81a5a134
ly <mikestyle05@gmail.com>
2023-12-20 22:21:26 -0500

    creating roberto v0.1 folder

---
+++ Chess-Challenge/src/Roberto v0.1-My Bot/MyBot.cs
@@ -0,0 +1,10 @@
+using ChessChallenge.API;
+
+public class MyBot : IChessBot
+{
+    public Move Think(Board board, Timer timer)
+    {
+        Move[] moves = board.GetLegalMoves();
+        return moves[0];
+    }
+}
